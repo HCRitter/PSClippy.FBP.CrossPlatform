3ca0e4553fe1d11ca2eaf0eddb8452547c03fe72
Christian Ritter <h.christian.ritter@gmail.com>
2024-04-16 11:42:40 +0200

    Imrpoved import logic

--- PSClippy.FBP.CrossPlatform.psm1
+++ PSClippy.FBP.CrossPlatform.psm1
@@ -4,11 +4,22 @@ try {
     if($null -ne (Get-Module -Name ScriptFeedbackProvider)){
         Import-Module ScriptFeedbackProvider -ErrorAction Stop
     }
+
 }
 catch {
     Write-Error 'Module: ScriptFeedBackProver  - must be installed!'
     return
 }
+
+try {
+    if((Get-ExperimentalFeature -Name PSFeedbackProvider).Enabled -eq $false){
+        Throw 'Experimental Feature PSFeedBackProvider is required'
+    }
+}
+catch {
+    return
+}
+
 Get-ScriptFeedbackProvider |Where-Object Name -eq 'Cross-Platform EnvironmentVariables' | Unregister-ScriptFeedbackProvider
 
 $EnvironmentVariablesScriptBlock=(get-command "$PSSCriptroot\Private\FBP.CP.Environment.ps1").ScriptBlock
